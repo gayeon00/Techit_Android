8ce9fcd58d7b3341c01c0b89d2acdef374fd90dc
gayeon00 <chlrkdus4819@gmail.com>
2023-06-29 23:08:28 +0900

    feat: Student 클래스 프로퍼티 추가

--- Android60_FragmentEX02/app/src/main/java/com/test/android60_fragmentex02/Student.kt
+++ Android60_FragmentEX02/app/src/main/java/com/test/android60_fragmentex02/Student.kt
@@ -1,13 +1,13 @@
 package com.test.android60_fragmentex02
 
-abstract class Student(val name: String)
+abstract class Student(val name: String, val team: String)
 
 class BaseballPlayer(name: String, val battingAvg: Double, val hCount: Int, val aCount: Int) :
-    Student(name)
+    Student(name, "야구")
 
-class SoccerPlayer(name: String, val goalCount: Int, val helpCount: Int) : Student(name)
+class SoccerPlayer(name: String, val goalCount: Int, val helpCount: Int) : Student(name, "축구")
 
-class SwimPlayer(name: String, val swimType: String) : Student(name)
+class SwimPlayer(name: String, val swimType: String) : Student(name, "수영")
 
 
 
