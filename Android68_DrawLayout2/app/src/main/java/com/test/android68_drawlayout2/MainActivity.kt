1eddd084441c486b204d788aefb2b277dd1a3426
gayeon00 <chlrkdus4819@gmail.com>
2023-07-03 15:21:12 +0900

    feat: fragment3개 추가

--- Android68_DrawLayout2/app/src/main/java/com/test/android68_drawlayout2/MainActivity.kt
+++ Android68_DrawLayout2/app/src/main/java/com/test/android68_drawlayout2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.test.android68_drawlayout2
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.LinearLayout
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,33 +12,50 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import com.test.android68_drawlayout2.databinding.ActivityMainBinding
+import com.test.android68_drawlayout2.databinding.NavHeaderMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+        setSupportActionBar(activityMainBinding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
+        //step01 =================================================
+
+        //nav_header_main.xml에 표시할 데이터 설정
+        //header부분의 linearlayout추출
+        val headerLayout = activityMainBinding.navView.getHeaderView(0) as LinearLayout
+        //하위 뷰 추출
+        //추출한 레이아웃이랑 binding클래스에 집어넣어주는 방법이라 bind
+        val headerBinding = NavHeaderMainBinding.bind(headerLayout)
+        headerBinding.imageViewHeader.setImageResource(R.drawable.imgflag8)
+        headerBinding.textViewHeader1.text = "한국 사람"
+        headerBinding.textViewHeader2.text = "나는 가끔 눈물을 흘린다...."
+
+        //step01 =================================================
+
+
+        activityMainBinding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+        val drawerLayout: DrawerLayout = activityMainBinding.drawerLayout
+        val navView: NavigationView = activityMainBinding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
