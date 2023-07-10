f329dd9db5c8b2960d3ba2fed927d0fe5c8dd58c
gayeon00 <chlrkdus4819@gmail.com>
2023-07-10 09:42:00 +0900

    refactor: memo 관련된 액티비티 모두 프래그먼트로 변경

---
+++ Android79_MiniProject02/app/src/main/java/com/test/android79_miniproject02/ui/memo/MainMemoFragment.kt
@@ -0,0 +1,109 @@
+package com.test.android79_miniproject02.ui.memo
+
+import android.content.Intent
+import android.graphics.Rect
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.test.android79_miniproject02.R
+import com.test.android79_miniproject02.dao.MemoDao
+import com.test.android79_miniproject02.data.Memo
+import com.test.android79_miniproject02.databinding.ActivityMemoListBinding
+import com.test.android79_miniproject02.databinding.FragmentMainMemoBinding
+import com.test.android79_miniproject02.databinding.RowMemoListBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainMemoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainMemoFragment : Fragment() {
+    lateinit var fragmentMainMemoBinding: FragmentMainMemoBinding
+    lateinit var memoListActivity: MemoListActivity
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentMainMemoBinding = FragmentMainMemoBinding.inflate(layoutInflater)
+        memoListActivity = activity as MemoListActivity
+
+
+        fragmentMainMemoBinding.run {
+            toolbarPwSetting.run {
+                title = memoListActivity.categoryName
+
+                //뒤로가기
+                setNavigationOnClickListener {
+                    memoListActivity.finish()
+                }
+
+                setOnMenuItemClickListener {
+                    //메모 추가
+                    memoListActivity.replaceFragment(MemoListActivity.ADD_MEMO_FRAGMENT, true, true)
+                    false
+                }
+            }
+
+            recyclerViewMemoList.run {
+                adapter = RecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(memoListActivity)
+                addItemDecoration(
+                    DividerItemDecoration(
+                        memoListActivity, DividerItemDecoration.VERTICAL
+                    )
+                )
+                addItemDecoration(CustomItemDecorator(53))
+            }
+        }
+        
+        return fragmentMainMemoBinding.root
+    }
+
+    inner class RecyclerViewAdapter :
+        RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
+        inner class RecyclerViewHolder(rowBinding: RowMemoListBinding) :
+            RecyclerView.ViewHolder(rowBinding.root) {
+            val textViewRowMemoTitle = rowBinding.textViewRowMemoTitle
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+            val rowBinding = RowMemoListBinding.inflate(layoutInflater)
+            val recyclerViewHolder = RecyclerViewHolder(rowBinding)
+
+            rowBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return recyclerViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return memoListActivity.memoList.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+            holder.textViewRowMemoTitle.text = memoListActivity.memoList[position].title
+        }
+    }
+
+    class CustomItemDecorator(private val desiredHeightInPixels: Int) :
+        RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
+        ) {
+            outRect.set(0, desiredHeightInPixels / 2, 0, desiredHeightInPixels / 2)
+        }
+    }
+
+}
--- Android79_MiniProject02/app/src/main/java/com/test/android79_miniproject02/ui/memo/MemoListActivity.kt
+++ Android79_MiniProject02/app/src/main/java/com/test/android79_miniproject02/ui/memo/MemoListActivity.kt
@@ -1,13 +1,18 @@
 package com.test.android79_miniproject02.ui.memo
 
+import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.test.android79_miniproject02.R
 import com.test.android79_miniproject02.dao.MemoDao
 import com.test.android79_miniproject02.data.Memo
 import com.test.android79_miniproject02.databinding.ActivityMemoListBinding
@@ -16,10 +21,18 @@ import com.test.android79_miniproject02.databinding.RowMemoListBinding
 class MemoListActivity : AppCompatActivity() {
     lateinit var activityMemoListBinding: ActivityMemoListBinding
 
-    private var categoryId = -1
-    private var categoryName = ""
+    var categoryId = -1
+    var categoryName = ""
 
-    private var memoList = listOf<Memo>()
+    var memoList = listOf<Memo>()
+
+    companion object {
+        // Activity가 관리할 프래그먼트들의 이름
+        const val MAIN_MEMO_FRAGMENT = "MainMemoFragment"
+        const val ADD_MEMO_FRAGMENT = "AddMemoFragment"
+        const val EDIT_MEMO_FRAGMENT = "EditMemoFragment"
+        const val SHOW_MEMO_FRAGMENT = "ShowMemoFragment"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         activityMemoListBinding = ActivityMemoListBinding.inflate(layoutInflater)
@@ -31,58 +44,54 @@ class MemoListActivity : AppCompatActivity() {
 
         memoList = MemoDao.getMemosByCategoryId(this, categoryId)
 
-        activityMemoListBinding.run {
-            toolbarPwSetting.run {
-                title = categoryName
-
-            }
-
-            recyclerViewMemoList.run {
-                adapter = RecyclerViewAdapter()
-                layoutManager = LinearLayoutManager(this@MemoListActivity)
-                addItemDecoration(
-                    DividerItemDecoration(
-                        this@MemoListActivity, DividerItemDecoration.VERTICAL
-                    )
-                )
-                addItemDecoration(CustomItemDecorator(53))
-            }
-        }
+        replaceFragment(MAIN_MEMO_FRAGMENT, false, false)
     }
 
-    inner class RecyclerViewAdapter :
-        RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
-        inner class RecyclerViewHolder(rowBinding: RowMemoListBinding) :
-            RecyclerView.ViewHolder(rowBinding.root) {
-            val textViewRowMemoTitle = rowBinding.textViewRowMemoTitle
+    fun replaceFragment(name: String, addToBackStack: Boolean, animate: Boolean) {
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        // 새로운 Fragment를 담을 변수
+        val newFragment = when (name) {
+            MAIN_MEMO_FRAGMENT -> {
+                MainMemoFragment()
+            }
+
+            ADD_MEMO_FRAGMENT -> {
+                AddMemoFragment()
+            }
+
+            EDIT_MEMO_FRAGMENT -> {
+                EditMemoFragment()
+            }
+
+            SHOW_MEMO_FRAGMENT -> {
+                ShowMemoFragment()
+            }
+
+            else -> {
+                Fragment()
+            }
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-            val rowBinding = RowMemoListBinding.inflate(layoutInflater)
-            val recyclerViewHolder = RecyclerViewHolder(rowBinding)
+        // Fragment를 교채한다.
+        fragmentTransaction.replace(R.id.memoListContainer, newFragment)
 
-            rowBinding.root.layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-
-            return recyclerViewHolder
+        if (animate) {
+            // 애니메이션을 설정한다.
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
         }
 
-        override fun getItemCount(): Int {
-            return memoList.size
+        if (addToBackStack) {
+            // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+            fragmentTransaction.addToBackStack(name)
         }
 
-        override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-            holder.textViewRowMemoTitle.text = memoList[position].title
-        }
+        // 교체 명령이 동작하도록 한다.
+        fragmentTransaction.commit()
     }
 
-    class CustomItemDecorator(private val desiredHeightInPixels: Int) :
-        RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(
-            outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
-        ) {
-            outRect.set(0, desiredHeightInPixels / 2, 0, desiredHeightInPixels / 2)
-        }
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name: String) {
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
